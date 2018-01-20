bc105747212a6cd176d7e8b0024ff1d90ee69800
SeanStar <i@seanstar.me>
2018-01-20 13:10:01 -0600

    Boost network testing

--- init/init_moto.cpp
+++ init/init_moto.cpp
@@ -53,28 +53,29 @@ void vendor_load_properties()
     property_set("ro.hw.radio", radio.c_str());
 
     //CDMA TESTING
-    std::string carrier = property_get("ro.boot.carrier");
-    std::string fsg = property_get("ro.boot.fsg-id");
-    cdma_properties();
-    if (carrier == "sprint") {
-        if (fsg == "boost") {
-	        property_set("ro.cdma.home.operator.numeric", "311870");
+    if (radio == "US") {
+        std::string carrier = property_get("ro.boot.carrier");
+        //std::string fsg = property_get("ro.boot.fsg-id"); //This seems to be useless unless specifically set in the oem
+        cdma_properties();
+        if (carrier == "sprint") {
+            //if (fsg == "boost") {
 	        property_set("ro.cdma.home.operator.alpha", "Boost Mobile");
+	        property_set("ro.cdma.home.operator.numeric", "311870");
+	        property_set("persist.omadm.operator.alpha", "Boost Mobile");
+	        property_set("persist.omadm.operator.numeric", "311870");
+            property_set("ro.com.google.clientidbase.am", "android-boost-us");
+            property_set("ro.com.google.clientidbase.ms", "android-boost-us");
+            property_set("ro.com.google.clientidbase.yt", "android-boost-us");
+            //}
+            property_set("ro.carrier", "sprint");
+            property_set("ro.mot.build.customerid ","sprint");
+        } else {
+            property_set("ro.telephony.get_imsi_from_sim", "true");
+            property_set("ro.cdma.data_retry_config", "max_retries=infinite,0,0,10000,10000,100000,10000,10000,10000,10000,140000,540000,960000");
+            property_set("ro.cdma.international.eri", "2,74,124,125,126,157,158,159,193,194,195,196,197,198,228,229,230,231,232,233,234,235");
+            property_set("ro.mot.build.customerid", "retus");
         }
-        property_set("ro.fsg-id", "sprint");
-        property_set("ro.carrier", "sprint");
-        property_set("ro.mot.build.customerid ","sprint");
-    } else {
-        property_set("ro.telephony.get_imsi_from_sim", "true");
-        property_set("ro.com.google.clientidbase.am", "android-verizon");
-        property_set("ro.com.google.clientidbase.ms", "android-verizon");
-        property_set("ro.com.google.clientidbase.yt", "android-verizon");
-        property_set("ro.cdma.data_retry_config", "max_retries=infinite,0,0,10000,10000,100000,10000,10000,10000,10000,140000,540000,960000");
-        property_set("ro.mot.build.customerid", "retus");
     }
-    property_set("persist.radio.multisim.config", "");
-    property_set("ro.cdma.international.eri", "2,74,124,125,126,157,158,159,193,194,195,196,197,198,228,229,230,231,232,233,234,235");
-    property_set("ro.com.android.dataroaming","false");
 }
 
 void cdma_properties()
@@ -98,7 +99,5 @@ void gsm_properties(bool msim)
         property_set("persist.radio.multisim.config", "dsds");
         property_set("persist.radio.plmn_name_cmp", "1");
         property_set("ro.telephony.ril.config", "simactivation");
-    } else {
-        property_set("persist.radio.multisim.config", "");
     }
 }
