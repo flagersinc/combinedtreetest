ee18292ddcee898e0a5692ad266707ae004a6de6
JasonM45 <45004662+JasonM45@users.noreply.github.com>
2018-12-06 07:59:10 -0500

    fix vendor.mk calls and changes for 15.1

--- device.mk
+++ device.mk
@@ -338,8 +338,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-#$(call inherit-product-if-exists, vendor/motorola/owens/owens-vendor.mk)
-$(call inherit-product-if-exists, vendor/motorola/msm8937-common/BoardConfigVendor.mk)
-$(call inherit-product-if-exists, vendor/motorola/qcom318-32/qcom318-32-vendor.mk)
+$(call inherit-product-if-exists, vendor/motorola/owens/owens-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
